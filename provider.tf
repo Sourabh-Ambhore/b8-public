5a54bc73d8f05519e37996ac3501d2f1f39708b1
Sourabh-Ambhore <ambhoresourabh98@gmail.com>
2024-11-18 10:51:19 +0530

    update

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = ""
-  secret_key = ""
+  access_key = "AKIA6ODU6OPCCCHD7FUB"
+  secret_key = "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"
 }
 
